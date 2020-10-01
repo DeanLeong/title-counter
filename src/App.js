1f9fc6ceb471f27f15a2ea45c45b4463609d1976
soleilyasmina <mounlasolomon@gmail.com>
2020-10-01 10:58:50 -0400

    final app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [dogImage, setDogImage] = useState("");
+
+  // run this useEffect when the component mounts
+  useEffect(() => {
+    const goFetch = async () => {
+      const dogResp = await axios.get(
+        "https://dog.ceo/api/breeds/image/random"
+      );
+      setDogImage(dogResp.data.message);
+    };
+    goFetch();
+  }, []);
+
+  // useEffect(cbfunction, dependencyArray)
+  useEffect(() => {
+    // if count is 10
+    if (count === 10) {
+      // make the API call
+      const goFetch = async () => {
+        const dogResp = await axios.get(
+          "https://dog.ceo/api/breeds/image/random"
+        );
+        setDogImage(dogResp.data.message);
+      };
+      goFetch();
+    } else {
+      setDogImage("");
+    }
+    // no matter what we want to change the document title
+    document.title = `${count} times clicked.`;
+  }, [count]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* condition ? (trueOutcome) : (falseOutcome) */}
+      {dogImage === "" ? (
+        <h3>You are not ready.</h3>
+      ) : (
+        <img src={dogImage} alt="doggo" />
+      )}
+      <button onClick={() => setCount(count + 1)}>CLICK ME</button>
     </div>
   );
 }
